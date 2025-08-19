c8cfaeba8408e6934a14f2b2aeb6542cdf67594b
msemly <msemly05@hotmail.com>
2025-08-20 09:25:04 +1000

    Delete keepalive.js

--- keepalive.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Bot is running!'));
-
-app.listen(3000, () => console.log('Keepalive server running on port 3000'));
